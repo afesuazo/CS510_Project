4cb6487bf6a0a31e9b3993ee8bf48197be1419cd
Andres Suazo <afesuazo@gmail.com>
2024-05-02 18:38:00 -0500

    Display video thumbnail in extension popup

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,3 +1,24 @@
+// Card with video thumbnail and title
+function createTopVideoCard(videoData) {
+    console.log('Creating video card for', videoData);
+    const clickable_card = document.createElement('a');
+    clickable_card.href = videoData.url;
+    clickable_card.target = '_blank';
+    clickable_card.className = 'video-card';
+
+    const title = document.createElement('h2');
+    title.textContent = videoData.title;
+    clickable_card.appendChild(title);
+
+    const thumbnail = document.createElement('img');
+    thumbnail.src = videoData.thumbnail;
+    thumbnail.alt = videoData.title + ' thumbnail';
+    thumbnail.style.width = '100%';
+    clickable_card.appendChild(thumbnail);
+
+    return clickable_card;
+}
+
 
 // When the popup is opened, check local storage for previous search recommendations and display them
 // Displayed elements are links to YouTube search queries
@@ -10,15 +31,38 @@ document.addEventListener('DOMContentLoaded', function () {
         const linksContainer = document.getElementById('links');
         if (result.searchRecommendations && result.searchRecommendations.length > 0) {
             result.searchRecommendations.forEach(recommendation => {
+
+                const container = document.createElement('div');
+                container.className = 'link-container';
+
                 // List element
                 const li = document.createElement('li');
                 // Anchor element to open the search query in a new tab
                 const a = document.createElement('a');
-                a.href = `https://www.youtube.com/results?search_query=${encodeURIComponent(recommendation)}`;
+                a.href = `https://www.youtube.com/results?search_query=${encodeURIComponent(recommendation.keyword)}`;
                 a.target = '_blank';
-                a.textContent = recommendation;
-                li.appendChild(a);
+                a.textContent = recommendation.keyword;
+                container.appendChild(a);
+
+                const videoToggleButton = document.createElement('button');
+                videoToggleButton.className = 'toggle-button';
+                videoToggleButton.innerHTML = 'Top Video <img src="../images/eye.png" alt="Show Video">';
+                container.appendChild(videoToggleButton);
+
+                li.appendChild(container);
                 linksContainer.appendChild(li);
+
+                // Add the top video card to the list element
+                const videoContainer = document.createElement('div');
+                const videoCard = createTopVideoCard(recommendation);
+                videoContainer.appendChild(videoCard);
+                videoContainer.className = 'video-container hidden';
+                li.appendChild(videoContainer);
+
+                videoToggleButton.onclick = () => {
+                    // Show the video card when the link is clicked
+                    videoContainer.classList.toggle('hidden');
+                }
             });
         } else {
             // Display a message if there are no results
